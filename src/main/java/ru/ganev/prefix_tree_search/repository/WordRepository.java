35fc2269a5a81ef2a5214400ddf72fd2caeb6a25
Artyom Ganev <a.a.ganev@gmail.com>
2019-05-18 10:42:12 +0300

    add working crud repository

--- src/main/java/ru/ganev/prefix_tree_search/repository/WordRepository.java
+++ src/main/java/ru/ganev/prefix_tree_search/repository/WordRepository.java
@@ -1,37 +1,82 @@
 package ru.ganev.prefix_tree_search.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.ganev.prefix_tree_search.model.Word;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import java.util.List;
+import java.util.Optional;
 
 @Repository
-public class WordRepository {
-
+public class WordRepository implements CrudRepository<Word, Long> {
+    @Autowired
     private EntityManager em;
 
-    public List<Word> findAll() {
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Word> cq = cb.createQuery(Word.class);
-        return em.createQuery(cq).getResultList();
+    @Override
+    @Transactional
+    public <S extends Word> S save(S s) {
+        return null;
     }
 
-    public List<Word> findWordsByStatusAndName(String name, String status) {
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Word> cq = cb.createQuery(Word.class);
-        Root<Word> book = cq.from(Word.class);
-        Predicate namePredicate = cb.equal(book.get("name"), name);
-        Predicate statusPredicate = cb.equal(book.get("status"), status);
-        cq.where(namePredicate, statusPredicate);
+    @Override
+    @Transactional
+    public <S extends Word> Iterable<S> saveAll(Iterable<S> iterable) {
+        return null;
+    }
 
-        TypedQuery<Word> query = em.createQuery(cq);
+    @Override
+    public Optional<Word> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public Iterable<Word> findAll() {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Word> cr = cb.createQuery(Word.class);
+        Root<Word> root = cr.from(Word.class);
+        cr.select(root);
+        TypedQuery<Word> query = em.createQuery(cr);
         return query.getResultList();
     }
 
+    @Override
+    public Iterable<Word> findAllById(Iterable<Long> iterable) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(Word word) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Word> iterable) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
 }
--- src/main/java/ru/ganev/prefix_tree_search/service/WordService.java
+++ src/main/java/ru/ganev/prefix_tree_search/service/WordService.java
@@ -5,15 +5,13 @@ import org.springframework.stereotype.Service;
 import ru.ganev.prefix_tree_search.model.Word;
 import ru.ganev.prefix_tree_search.repository.WordRepository;
 
-import java.util.List;
-
 @Service
 public class WordService implements IWordService {
     @Autowired
     private WordRepository repository;
 
     @Override
-    public List<Word> findAll() {
+    public Iterable<Word> findAll() {
         return repository.findAll();
     }
 }
